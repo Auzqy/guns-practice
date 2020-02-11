9640ab5362102de9b7c0da2043b78b842f398318
au <auzhouqingyu@163.com>
2020-02-11 18:27:06 +0800

    coding HouseController

--- guns-rest/src/main/java/com/stylefeng/guns/rest/modular/house/controller/HouseController.java
+++ guns-rest/src/main/java/com/stylefeng/guns/rest/modular/house/controller/HouseController.java
@@ -1,6 +1,11 @@
 package com.stylefeng.guns.rest.modular.house.controller;
 
 
+import com.stylefeng.guns.rest.modular.house.service.IHouseService;
+import com.stylefeng.guns.rest.persistence.model.House;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
@@ -15,7 +20,16 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 @RequestMapping("/house")
+@Slf4j
 public class HouseController {
 
+    @Autowired
+    private IHouseService houseService;
+
+    @RequestMapping("/hello")
+    public House hello(@RequestBody House house) {
+        log.info("house: {}",house);
+        return house;
+    }
 }
 
